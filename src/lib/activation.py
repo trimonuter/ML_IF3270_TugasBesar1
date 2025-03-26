fix: tanh derivative function

src/lib/activation.py
@@ -20,6 +20,10 @@ def sigmoid(x):
 def tanh(x):
     return np.tanh(x)
 
+@np.vectorize
+def derivative_tanh(x):
+    return (2 / (np.exp(x) + np.exp(-x))) ** 2
+
 # Softmax
 def softmax(x):
     pass
@@ -33,7 +37,7 @@ def getDerivativeMatrix(activation, matrix):
     elif activation == sigmoid:
         return matrix * (1 - matrix)
     elif activation == tanh:
-        return 1 - matrix ** 2
+        return derivative_tanh(matrix)
     elif activation == softmax:
         pass
     else:

src/main_testing_1_noweights.py
@@ -7,10 +7,10 @@ X = np.array([[0.05, 0.10]])
 target = np.array([[0.01, 0.99]])
 
 model = FFNN.FFNN([2, 2, 2], X, target, 0.5)
-model.setActivationUniform(Activation.linear)
+model.setActivationUniform(Activation.tanh)
 model.initializeWeightRandomUniform(-1, 1)
 
-for i in range(1000):
+for i in range(500):
     model.FFNNForwardPropagation()
     
     print(f'Epoch {i+1} Prediction:')