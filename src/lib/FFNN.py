3bc9b505a6c7f42d007307a81455e2bbf98a5e3a
[pol] Muhammad Rasheed Qais Tandjung <13522158@std.stei.itb.ac.id>
2025-03-26 11:31:05 +0700

    feat: train method with batch sizes

--- src/lib/FFNN.py
+++ src/lib/FFNN.py
@@ -1,14 +1,16 @@
 from lib import matrix as Matrix
 from lib import activation as Activation
+from lib import loss as Loss
+from lib import color as Color
 import numpy as np
 
 class FFNN:
     # Static attributes
 
-    def __init__(self, layer_neurons, input, target, learning_rate, activation_functions=None):
+    def __init__(self, layer_neurons, X_train, y_train, learning_rate, activation_functions=None, X_val=None, y_val=None):
         self.layer_neurons = layer_neurons
-        self.input = input
-        self.target = target
+        self.input = X_train
+        self.target = y_train
         self.learning_rate = learning_rate
         self.activations = activation_functions
 
@@ -17,6 +19,10 @@ class FFNN:
 
         # For weight initialization
         self.sizes = [(self.layer_neurons[i] + 1, self.layer_neurons[i + 1]) for i in range(len(self.layer_neurons) - 1)]
+
+        # For validation
+        self.X_val = X_val
+        self.y_val = y_val
     
     def setLearningRate(self, learning_rate):
         self.learning_rate = learning_rate
@@ -40,10 +46,10 @@ class FFNN:
             np.random.seed(seed)
         self.weights = [np.random.normal(mean, variance, size) for size in self.sizes]
 
-    def FFNNForwardPropagation(self):
-        self.layer_results = [self.input]
-        self.layer_results_before_activation = [self.input]
-        input = Matrix.addBiasColumn(self.input)
+    def FFNNForwardPropagation(self, current_input):
+        self.layer_results = [current_input]
+        self.layer_results_before_activation = [current_input]
+        input = Matrix.addBiasColumn(current_input)
         i = 1
         for layer in self.weights:
             # Get layer result
@@ -64,7 +70,7 @@ class FFNN:
             if i > len(self.weights):
                 return result
 
-    def FFNNBackPropagation(self):
+    def FFNNBackPropagation(self, current_target):
         deltas = []
         delta_weights = []
         n = len(self.weights)
@@ -75,7 +81,7 @@ class FFNN:
 
             if i == n:
                 # Output layer
-                delta = (self.target - output) * Activation.getDerivativeMatrix(self.activations[i], output)
+                delta = (current_target - output) * Activation.getDerivativeMatrix(self.activations[i], output)
             else:
                 # Hidden layer
                 weight_ds = Matrix.removeBiasRow(self.weights[i])           # Downstream weight (Wkj) matrix
@@ -91,8 +97,47 @@ class FFNN:
 
             delta_weights = [weight_change] + delta_weights
 
+        # Set current epoch's gradient array
+        self.deltas = deltas
+
         # Update old weights after backpropagation has finished
         for i, weight_change in enumerate(delta_weights):
             self.weights[i] += weight_change
 
-    
+    def train(self, batch_size, learning_rate, epochs, verbose=True, printResults=False):
+        self.setLearningRate(learning_rate)
+        training_loss_list = []
+        validation_loss_list = []
+
+        for epoch in range(epochs):
+            training_loss = 0
+            validation_loss = 0
+            
+            # Iterate through batches
+            for i in range(0, len(self.input), batch_size):
+                batch_end = (i + batch_size) if (i + batch_size) < len(self.input) else len(self.input)
+                X_batch = self.input[i:batch_end]
+                y_batch = self.target[i:batch_end]
+
+                self.FFNNForwardPropagation(X_batch)
+                self.FFNNBackPropagation(y_batch)
+
+                training_loss += Loss.mse(y_batch, self.layer_results[-1])
+
+            # Calculate epoch loss
+            training_loss /= len(self.input)
+            training_loss_list.append(training_loss)
+
+            if self.X_val != None and self.y_val != None:
+                self.FFNNForwardPropagation(self.X_val)
+                validation_loss = Loss.mse(self.y_val, self.layer_results[-1])
+                validation_loss_list.append(validation_loss)
+
+            # Print epoch results
+            if verbose:
+                print(Color.YELLOW + f"[Epoch {epoch + 1}]:" + Color.GREEN + f" Training Loss: {training_loss}" + Color.BLUE + f" Validation Loss: {validation_loss}" + Color.RESET)
+                if printResults:
+                    print(f"{Color.CYAN}     Prediction: {self.layer_results[-1]}")
+                    print(f"{Color.MAGENTA}     Target: {self.target}{Color.RESET}")
+
+        return training_loss_list, validation_loss_list
