ab3cd4b6365b86c4975252af54724dcc0adb1337
LastPrism7 <67941358+LastPrism7@users.noreply.github.com>
2025-03-26 23:51:34 +0700

    feat: bce

--- src/lib/loss.py
+++ src/lib/loss.py
@@ -5,5 +5,9 @@ def mse(target, prediction):
     return np.mean((target - prediction) ** 2)
 
 # Binary Cross Entropy
+def bce(target, prediction):
+    epsilon = 1e-12 # avoiding log(0) errors
+    prediction = np.clip(prediction, epsilon, 1 - epsilon)
+    return -np.mean(target * np.log(prediction) + (1 - target) * np.log(1 - prediction))
 
 # Categorical Cross Entropy
