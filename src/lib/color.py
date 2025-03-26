711a3f183ef268e735c64e2a8ce5ecebffd759a1
[pol] Muhammad Rasheed Qais Tandjung <13522158@std.stei.itb.ac.id>
2025-03-26 11:29:53 +0700

    style: add color library

---
+++ src/lib/color.py
@@ -0,0 +1,15 @@
+# ANSI color codes as constants
+
+RESET = "\033[0m"
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+
+# Foreground colors
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
