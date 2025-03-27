bb95666809f397880a4ccb0b04e62ea019a2bb09
MelonSeed9 <andisitorusman@gmail.com>
2025-03-28 06:23:28 +0700

    feat: derivative cce

--- src/lib/loss.py
+++ src/lib/loss.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from lib import activation as Activation
 
 # MSE
 def mse(target, prediction):
@@ -12,7 +13,13 @@ def bce(target, prediction):
     return -np.mean(target * np.log(prediction) + (1 - target) * np.log(1 - prediction))
 
 # Categorical Cross Entropy
-def cce(target, prediction, n):
+def cce(target, prediction):
     epsilon = 1e-12 # avoiding log(0) errors    
     prediction = np.clip(prediction, epsilon, 1 - epsilon)
     return -np.mean(np.sum(target * np.log(prediction)), axis=1)
+def categorical_cross_entropy_gradient(X, y_true, W):
+    input = np.dot(X, W)  # Output sebelum softmax
+    y_pred = Activation.softmax(input)  # Softmax probabilities
+    dL_dy = (y_pred - y_true) / X.shape[0]  
+    dW = np.dot(X.T, dL_dy)
+    return dW
